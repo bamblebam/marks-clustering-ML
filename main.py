8da526705077ca3060f555b3b4e75036411ece2d
bamblebam <burhanizrangwala@gmail.com>
2021-02-28 04:51:00 +0530

    clustering dict basic

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
+from collections import defaultdict
 # %%
 df = pd.read_csv('datasets/processed.csv')
 # %%
@@ -16,6 +17,30 @@ Y = df.drop(clustering_cols, axis=1)
 kmeans = KMeans(n_clusters=44)
 kmeans.fit(X)
 # %%
-print(kmeans.labels_)
+labels = kmeans.labels_
 
+# %%
+clusters = pd.DataFrame({"clusters": labels})
+new_df = pd.concat([clusters, df], axis=1)
+# %%
+new_df.head()
+# %%
+new_df["K.J.Somaiya College of Engineering, Mumbai - 77, .-."] = new_df[
+    "K.J.Somaiya College of Engineering, Mumbai - 77, .-."].apply(str)
+new_df["Unnamed: 1"] = new_df["Unnamed: 1"].apply(str)
+
+# %%
+name_list = list()
+for index, row in new_df.iterrows():
+    name_list.append(
+        row["K.J.Somaiya College of Engineering, Mumbai - 77, .-."]+" "+row["Unnamed: 1"])
+# %%
+cluster_dict = defaultdict(list)
+for index, cluster_num in enumerate(labels):
+    cluster_dict[cluster_num].append(name_list[index])
+# %%
+cluster_dict = dict(cluster_dict)
+cluster_dict = dict(sorted(cluster_dict.items(), key=lambda x: x[0]))
+# %%
+print(cluster_dict)
 # %%
